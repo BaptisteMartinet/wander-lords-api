7aeec00a0888bdb64b80721e908a979bf79a76da
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-11-26 13:59:36 +0100

    refactor: main function

--- src/index.ts
+++ src/index.ts
@@ -7,43 +7,47 @@ import { WebSocketServer } from 'ws';
 import { useServer } from 'graphql-ws/lib/use/ws';
 import cors from 'cors';
 import sequelize from './core/sequelize.js';
-import schema from './schema/index.js'
+import schema from '@schema/index.js';
 
-await sequelize.authenticate();
+async function main() {
+  await sequelize.authenticate();
 
-// TODO migration script
-await sequelize.sync({ force: true });
-console.warn('Database synchronized');
+  // TODO migration script
+  await sequelize.sync({ force: true });
+  console.warn('Database synchronized');
 
-const app = express();
-const httpServer = createServer(app);
+  const app = express();
+  const httpServer = createServer(app);
 
-const wsServer = new WebSocketServer({
-  server: httpServer,
-  path: '/graphql',
-});
+  const wsServer = new WebSocketServer({
+    server: httpServer,
+    path: '/graphql',
+  });
 
-const serverCleanup = useServer({ schema }, wsServer);
-const server = new ApolloServer({
-  schema,
-  plugins: [
-    ApolloServerPluginDrainHttpServer({ httpServer }),
-    {
-      async serverWillStart() {
-        return {
-          async drainServer() {
-            await serverCleanup.dispose();
-          },
-        };
+  const serverCleanup = useServer({ schema }, wsServer);
+  const server = new ApolloServer({
+    schema,
+    plugins: [
+      ApolloServerPluginDrainHttpServer({ httpServer }),
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              await serverCleanup.dispose();
+            },
+          };
+        },
       },
-    },
-  ],
-});
+    ],
+  });
 
-await server.start();
-app.use('/graphql', cors<cors.CorsRequest>(), express.json(), expressMiddleware(server));
+  await server.start();
+  app.use('/graphql', cors<cors.CorsRequest>(), express.json(), expressMiddleware(server));
 
-const PORT = 4000;
-httpServer.listen(PORT, () => {
-  console.log(`Server started at http://localhost:${PORT}/graphql 🚀`);
-});
+  const PORT = 4000;
+  httpServer.listen(PORT, () => {
+    console.log(`Server started at http://localhost:${PORT}/graphql 🚀`);
+  });
+}
+
+main().catch(console.error);
