77425395837cafc05e915649685400f34307dbab
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-11-26 02:32:49 +0100

    refactor: minor

--- src/core/sequelize.ts
+++ src/core/sequelize.ts
@@ -1,8 +1,6 @@
 import { Sequelize } from 'sequelize';
 import { DATABASE_URL } from './env.js';
 
-console.log(DATABASE_URL);
-
 const sequelize = new Sequelize(DATABASE_URL);
 
 export default sequelize;
