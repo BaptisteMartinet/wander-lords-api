7735003be74252d3b103401c6ea6da3b84db3502
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-11-28 00:04:53 +0100

    feat(definitions/models): rework User model

--- src/definitions/models/User.ts
+++ src/definitions/models/User.ts
@@ -1,14 +1,31 @@
+import type {
+  Model as SequelizeModel,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+
 import Model, { Int, String, Boolean } from '@lib/model';
 import sequelize from '@core/sequelize.js';
 
-export default new Model({
+export interface UserInstance extends SequelizeModel<InferAttributes<UserInstance>, InferCreationAttributes<UserInstance>> {
+  id: CreationOptional<number>;
+  testInt: number;
+  testString: string;
+  testUnexposed: number;
+  testBool: boolean;
+}
+
+const User = new Model<UserInstance>({
   name: 'User',
   fields: {
     testInt: { type: Int, allowNull: true, exposed: true },
-    testString: { type: String, allowNull: false, exposed: true, description: 'coucou'},
+    testString: { type: String, allowNull: false, exposed: true, description: 'coucou' },
     testUnexposed: { type: Int, allowNull: false, exposed: false },
     testBool: { type: Boolean, allowNull: false, defaultValue: 12, exposed: true },
   },
   timestamps: true,
   sequelize,
 });
+
+export default User;
