c234e39c85a2fb09e7893a164696381737706388
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-11-29 18:58:00 +0100

    feat(lib/utils): improve filterRecord typings

--- src/lib/utils/object.ts
+++ src/lib/utils/object.ts
@@ -8,8 +8,8 @@ export function mapRecord<K extends string, T, U>(obj: Record<K, T>, fn: (value:
   return newObj;
 }
 
-export function filterRecord<K extends string, T>(obj: Record<K, T>, fn: (value: T) => boolean) {
-  const newObj: Record<string, T> = {};
+export function filterRecord<K extends string, U, T extends Record<K, U>>(obj: T, fn: (value: U) => boolean) {
+  const newObj: Partial<T> = {};
   for (const key in obj) {
     const value = obj[key];
     if (fn(value))
