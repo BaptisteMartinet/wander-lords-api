644ed19d2c39dd15e4c6c15da90e4f4361a11bf1
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-11-30 21:27:40 +0100

    [BRK] feat(lib/utils): change getEnumEntries function name

--- src/lib/utils/enum.ts
+++ src/lib/utils/enum.ts
@@ -3,7 +3,7 @@ import { filterRecord } from './object';
 export type EnumValueType = number | string;
 export type EnumType<KeyType extends string = string> = Record<KeyType, EnumValueType>;
 
-export function enumEntries<K extends string>(value: EnumType<K>)
+export function getEnumEntries<K extends string>(value: EnumType<K>)
 {
   return filterRecord(value, (_, key) => isNaN(Number(key))) as EnumType<K>;
 }
