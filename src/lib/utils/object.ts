fe8b8ce223e39e3cfb8e8107308e9a4443c4dcbe
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-12-17 23:02:37 +0100

    feat(lib/utils): implement reduceRecord util

--- src/lib/utils/object.ts
+++ src/lib/utils/object.ts
@@ -21,6 +21,19 @@ export function filterRecord<K extends string, V>(obj: Record<K, V>, fn: (value:
   return newObj;
 }
 
+export function reduceRecord<
+  KeyType extends string,
+  ValueType,
+  OutputType,
+>(obj: Record<KeyType, ValueType>, fn: (prev: OutputType, current: ValueType, key: KeyType) => OutputType, initialValue: OutputType): OutputType {
+  let acc = initialValue;
+  for (const key in obj) {
+    const value = obj[key];
+    acc = fn(acc, value, key);
+  }
+  return acc;
+}
+
 export function makeRecordFromEntries<K extends string, V>(entries: Iterable<[ K, V ]>): Record<K, V> {
   return Array.from(entries).reduce((acc, [key, val]) => {
     acc[key] = val;
