346cdf1b54b9c9c2c541f4224c45ace2a20d780e
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-12-28 22:39:28 +0100

    feat(core): remove sequelize logging deprecation warning

--- src/core/sequelize.ts
+++ src/core/sequelize.ts
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize';
 import { DATABASE_URL, DISABLE_LOGGING } from './env.js';
 
 const sequelize = new Sequelize(DATABASE_URL, {
-  logging: DISABLE_LOGGING !== true,
+  logging: DISABLE_LOGGING !== true ? console.log : false,
 });
 
 export default sequelize;
