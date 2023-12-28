00baa0906be3193b6bbe83987f42ec68ea6ace02
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-12-28 22:36:35 +0100

    fix(core): fix sequelize logging

--- src/core/sequelize.ts
+++ src/core/sequelize.ts
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize';
 import { DATABASE_URL, DISABLE_LOGGING } from './env.js';
 
 const sequelize = new Sequelize(DATABASE_URL, {
-  logging: DISABLE_LOGGING === false,
+  logging: DISABLE_LOGGING !== true,
 });
 
 export default sequelize;
