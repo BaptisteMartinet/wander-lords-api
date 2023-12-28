116f88ae31842a1caf7603d725c421a45b4a0989
Baptiste Martinet <baptiste.martinet@epitech.eu>
2023-12-28 23:36:57 +0100

    feat(schema/mutation): minor

--- src/schema/mutation/index.ts
+++ src/schema/mutation/index.ts
@@ -1,11 +1,11 @@
-import { GraphQLObjectType } from 'graphql';
+import { GraphQLNonNull, GraphQLObjectType } from 'graphql';
 import AuthorMutation from './Author';
 import BookMutation from './Book';
 
 export default new GraphQLObjectType({
   name: 'Mutation',
   fields: {
-    author: { type: AuthorMutation, resolve: () => ({}) },
-    book: { type: BookMutation, resolve: () => ({}) },
+    author: { type: new GraphQLNonNull(AuthorMutation), resolve: () => ({}) },
+    book: { type: new GraphQLNonNull(BookMutation), resolve: () => ({}) },
   },
 });
