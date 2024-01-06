aa953d36c5624d64910bccda3f79de4af9039426
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-01-06 23:56:46 +0100

    feat(lib/schema): implement genModelOrderBy

---
+++ src/lib/schema/genModelOrderBy.ts
@@ -0,0 +1,45 @@
+import type { Model as SequelizeModel, OrderItem } from 'sequelize';
+
+import { GraphQLEnumType, GraphQLInputObjectType, GraphQLNonNull } from 'graphql';
+import { Model } from '@lib/definitions';
+import { mapRecord } from '@lib/utils/object';
+
+export enum OrderType {
+  ASC = 'ASC',
+  DESC = 'DESC',
+}
+
+export const OrderTypeEnum = new GraphQLEnumType({
+  name: 'OrderType',
+  values: {
+    ASC: { value: 'ASC' },
+    DESC: { value: 'DESC' },
+  },
+});
+
+export function genModelFieldsEnum<M extends SequelizeModel>(model: Model<M>) {
+  return new GraphQLEnumType({
+    name: model.name + 'Fields',
+    values: mapRecord(model.model.getAttributes(), (_, key) => ({ value: key })),
+  });
+}
+
+export interface GenericOrderBy {
+  field: string;
+  ordering: OrderType;
+}
+
+export default function genModelOrderBy<M extends SequelizeModel>(model: Model<M>) {
+  return new GraphQLInputObjectType({
+    name: model.name + 'OrderBy',
+    fields: {
+      field: { type: new GraphQLNonNull(genModelFieldsEnum(model)) },
+      ordering: { type: new GraphQLNonNull(OrderTypeEnum) }
+    },
+  });
+}
+
+export function convertOrderByToSequelizeOrderItem(orderBy: GenericOrderBy): OrderItem {
+  const { field, ordering } = orderBy;
+  return [field, ordering];
+}
--- src/lib/schema/index.ts
+++ src/lib/schema/index.ts
@@ -1,4 +1,11 @@
 export { default as exposeModel } from './exposeModel';
 export { default as scopedMutation } from './scopedMutation';
 export { default as genModelOffsetPagination } from './offset-pagination';
+export {
+  default as genModelOrderBy,
+  convertOrderByToSequelizeOrderItem,
+  OrderTypeEnum,
+  type OrderType,
+  type GenericOrderBy,
+} from './genModelOrderBy';
 export * from './context';
