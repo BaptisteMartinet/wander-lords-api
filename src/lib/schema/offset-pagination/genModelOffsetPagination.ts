14e25f8b79a1891db9e79681a75e58b849abe914
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-01-06 23:57:08 +0100

    feat(lib/schema): implement order on genModelOffsetPagination

--- src/lib/schema/offset-pagination/genModelOffsetPagination.ts
+++ src/lib/schema/offset-pagination/genModelOffsetPagination.ts
@@ -1,13 +1,16 @@
 import type { Model as SequelizeModel } from 'sequelize';
 import type { GraphQLFieldConfig } from 'graphql';
 import type { Model } from '@lib/definitions';
+import type { GenericOrderBy } from '@lib/schema';
 
 import { GraphQLInt, GraphQLNonNull, GraphQLObjectType } from 'graphql';
 import { GraphQLNonNullList } from '@lib/graphql';
+import { genModelOrderBy, convertOrderByToSequelizeOrderItem } from '@lib/schema';
 
 interface OffsetPaginationArgs {
   offset?: number | null,
   limit?: number | null,
+  order?: GenericOrderBy[] | null,
 }
 
 export default function genModelOffsetPagination<M extends SequelizeModel>(model: Model<M>): GraphQLFieldConfig<unknown, unknown, OffsetPaginationArgs> {
@@ -16,12 +19,14 @@ export default function genModelOffsetPagination<M extends SequelizeModel>(model
     args: {
       offset: { type: GraphQLInt },
       limit: { type: GraphQLInt },
+      order: { type: new GraphQLNonNullList(genModelOrderBy(model)) },
     },
     async resolve(_, args) {
-      const { offset, limit } = args;
+      const { offset, limit, order } = args;
       const { rows: nodes, count } = await model.model.findAndCountAll({
         offset: offset ?? undefined,
         limit: limit ?? undefined,
+        order: order?.map(convertOrderByToSequelizeOrderItem),
       });
       return { nodes, count };
     },
