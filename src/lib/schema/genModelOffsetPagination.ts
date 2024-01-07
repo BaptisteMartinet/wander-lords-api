7ae89d56c66aeb575e43003d33b558c8884eb3d1
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-01-07 16:11:45 +0100

    feat(lib/schema): implement args and where on genModelOffsetPagination

--- src/lib/schema/genModelOffsetPagination.ts
+++ src/lib/schema/genModelOffsetPagination.ts
@@ -1,5 +1,6 @@
-import type { Model as SequelizeModel } from 'sequelize';
-import type { GraphQLFieldConfig, GraphQLOutputType } from 'graphql';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import type { Model as SequelizeModel, WhereOptions, Attributes } from 'sequelize';
+import type { GraphQLFieldConfig, GraphQLFieldConfigArgumentMap, GraphQLNamedOutputType, GraphQLOutputType } from 'graphql';
 import type { Model } from '@lib/definitions';
 import type { GenericOrderBy } from '@lib/schema';
 
@@ -11,29 +12,39 @@ export interface OffsetPaginationGraphQLArgs {
   offset?: number | null,
   limit?: number | null,
   order?: GenericOrderBy[] | null,
+  [key: string]: unknown, // Custom args
 }
 export type OffsetPaginationGraphQLFieldConfig = GraphQLFieldConfig<unknown, unknown, OffsetPaginationGraphQLArgs>;
 
-export interface OffsetPaginationOpts {
-  outputType?: GraphQLOutputType,
+export interface OffsetPaginationOpts<M extends SequelizeModel> {
+  outputType?: GraphQLNamedOutputType,
+  args?: GraphQLFieldConfigArgumentMap,
+  where?: (args: any) => WhereOptions<Attributes<M>>,
 }
 
-export default function genModelOffsetPagination<M extends SequelizeModel>(model: Model<M>, opts: OffsetPaginationOpts = {}): OffsetPaginationGraphQLFieldConfig {
-  const { outputType } = opts;
+export default function genModelOffsetPagination<M extends SequelizeModel>(
+  model: Model<M>,
+  opts: OffsetPaginationOpts<M> = {}
+): OffsetPaginationGraphQLFieldConfig {
+  const { outputType, args, where: whereGetter } = opts;
   const nodeType = outputType ?? model.type;
+  const name = nodeType.name;
   return {
-    type: makeOffsetConnection(model.name, nodeType),
+    type: makeOffsetConnection(name, nodeType),
     args: {
+      ...args,
       offset: { type: GraphQLInt },
       limit: { type: GraphQLInt },
       order: { type: new GraphQLNonNullList(genModelOrderBy(model)) },
     },
     async resolve(_, args) {
-      const { offset, limit, order } = args;
+      const { offset, limit, order, ...customArgs } = args;
+      const where = whereGetter?.(customArgs);
       const { rows: nodes, count } = await model.model.findAndCountAll({
         offset: offset ?? undefined,
         limit: limit ?? undefined,
         order: order?.map(convertOrderByToSequelizeOrderItem),
+        where: where,
       });
       return { nodes, count };
     },
