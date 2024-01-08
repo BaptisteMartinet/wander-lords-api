9453f508ed67fe7ea826e4db85593b6f7eb1e9df
Baptiste Martinet <baptiste.martinet@epitech.eu>
2024-01-08 23:16:26 +0100

    feat(lib/definitions): minor

--- src/lib/definitions/model/constants.ts
+++ src/lib/definitions/model/constants.ts
@@ -3,4 +3,4 @@ import type { IDFieldDefinition } from './types';
 import { DataTypes } from 'sequelize';
 import { ID } from './fieldTypes';
 
-export const DefaultIDFieldDefinition: IDFieldDefinition = { type: ID, defaultValue: DataTypes.UUIDV4 };
+export const DefaultIDFieldDefinition: IDFieldDefinition = { type: ID, defaultValue: DataTypes.UUIDV4 } as const;
